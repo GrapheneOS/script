a4a967ceeca5a23aa3e578c58fce2a6fd220ddc5
Daniel Micay <danielmicay@gmail.com>
2018-11-21 16:15:53 -0500

    switch LANG to en_US.UTF-8

--- envsetup.sh
+++ envsetup.sh
@@ -1,6 +1,6 @@
 source build/envsetup.sh
 
-export LANG=C
+export LANG=en_US.UTF-8
 export _JAVA_OPTIONS=-XX:-UsePerfData
 export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date --utc +%Y.%m.%d.%H)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
