a39ca5f1ce37ec12e23b292ce11727250718ada9
Daniel Micay <danielmicay@gmail.com>
2019-04-29 10:04:05 -0400

    manage kernel_google_marlin again

--- manage.sh
+++ manage.sh
@@ -67,7 +67,7 @@ aosp_forks=(
 )
 
 declare -A kernels=(
-  #[google_marlin]=android-9.0.0_r0.71 # April 2019
+  [google_marlin]=android-9.0.0_r0.71 # April 2019
   [google_wahoo]=android-9.0.0_r0.72 # April 2019
   [google_crosshatch]=android-9.0.0_r0.73 # April 2019
   #[linaro_hikey]=dc721a4ac71d
