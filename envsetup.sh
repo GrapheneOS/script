02598e24dc03571a452fcf293273c9f59eab2d12
Felix Jankowski <felix@jankowski.me>
2019-05-12 22:14:34 +0200

    Modify script/envsetup.sh to support building on Mac OS

--- envsetup.sh
+++ envsetup.sh
@@ -2,8 +2,10 @@ source build/envsetup.sh
 
 export LANG=en_US.UTF-8
 export _JAVA_OPTIONS=-XX:-UsePerfData
-export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date --utc +%Y.%m.%d.%H)
+export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date -u +%Y.%m.%d.%H)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
-chrt -b -p 0 $$
+if [[ `uname` != 'Darwin' ]]; then
+    chrt -b -p 0 $$
+fi
 export PATH="$PWD/script/bin:$PATH"
