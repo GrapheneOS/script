bd84f9a85ae33abb4521507f1db32a0a5b932168
Daniel Micay <danielmicay@gmail.com>
2019-07-01 16:10:16 -0400

    update kernel tags for July

--- manage.sh
+++ manage.sh
@@ -64,10 +64,10 @@ aosp_forks=(
 )
 
 declare -A kernels=(
-  [google_marlin]=android-9.0.0_r0.91 # June 2019
-  [google_wahoo]=android-9.0.0_r0.92 # June 2019
-  [google_crosshatch]=android-9.0.0_r0.93 # June 2019
-  [google_bonito]=android-9.0.0_r0.94 # June 2019
+  [google_marlin]=android-9.0.0_r0.95 # July 2019
+  [google_wahoo]=android-9.0.0_r0.96 # July 2019
+  [google_crosshatch]=android-9.0.0_r0.98 # July 2019
+  [google_bonito]=android-9.0.0_r0.100 # July 2019
   #[linaro_hikey]=dc721a4ac71d
 )
 
