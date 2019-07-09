d858b6d42b97ae66183fc625b164f3d1d3582725
Daniel Micay <danielmicay@gmail.com>
2019-07-09 17:10:29 -0400

    use `date --utc` -> `date -u` for portability

--- envsetup.sh
+++ envsetup.sh
@@ -2,7 +2,7 @@ source build/envsetup.sh
 
 export LANG=en_US.UTF-8
 export _JAVA_OPTIONS=-XX:-UsePerfData
-export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date --utc +%Y.%m.%d.%H)
+export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date -u +%Y.%m.%d.%H)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
 chrt -b -p 0 $$
