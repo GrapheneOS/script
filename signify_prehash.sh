0cbdc2d9fab8d2a7d02fa271719f37060ef6c596
Daniel Micay <danielmicay@gmail.com>
2019-07-18 21:11:01 -0400

    avoid leading path in sha256sum output

--- signify_prehash.sh
+++ signify_prehash.sh
@@ -2,4 +2,9 @@
 
 [[ $# -eq 2 ]] || exit 1
 
-sha256sum --tag $2 | signify -S -s $1 -e -m - -x $2.sig
+key="$(realpath $1)"
+file=$(basename $2)
+
+cd "$(dirname $2)"
+
+sha256sum --tag "$file" | signify -S -s "$key" -e -m - -x "$file.sig"
