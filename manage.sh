209933264cf5aae1c3dc90e7cc0584b536888999
Daniel Micay <danielmicay@gmail.com>
2019-09-26 20:03:40 -0400

    no longer need platform_development

--- manage.sh
+++ manage.sh
@@ -33,7 +33,6 @@ aosp_forks=(
   platform_bootable_recovery
   platform_build
   platform_build_soong
-  platform_development
   #platform_external_clang
   platform_external_conscrypt
   #platform_external_llvm
