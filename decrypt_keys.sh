e67b5ce08f3a7d4a4641f8fa629e051b7e6c1d07
Daniel Micay <danielmicay@gmail.com>
2020-03-17 21:08:26 -0400

    decrypt_keys: handle unencrypted keys

--- decrypt_keys.sh
+++ decrypt_keys.sh
@@ -22,12 +22,16 @@ export password
 for key in releasekey platform shared media networkstack; do
     if [[ -n $password ]]; then
         openssl pkcs8 -in $key.pk8 -inform DER -passin env:password | openssl pkcs8 -topk8 -outform DER -out "$tmp/$key.pk8" -nocrypt
+    else
+        openssl pkcs8 -topk8 -inform DER -in $key.pk8 -outform DER -out "$tmp/$key.pk8" -nocrypt
     fi
 done
 
 if [[ -f avb.pem ]]; then
     if [[ -n $password ]]; then
         openssl pkcs8 -topk8 -in avb.pem -passin env:password -out "$tmp/avb.pem" -nocrypt
+    else
+        openssl pkcs8 -topk8 -in avb.pem -out "$tmp/avb.pem" -nocrypt
     fi
 fi
 
