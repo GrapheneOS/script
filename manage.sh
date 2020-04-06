9835e6f1c50fcca1dd042438eb4a1be8b6eecb26
Daniel Micay <danielmicay@gmail.com>
2020-04-06 15:22:23 -0400

    update to QQ2A.200405.005

--- manage.sh
+++ manage.sh
@@ -14,9 +14,9 @@ elif [[ $# -ne 0 ]]; then
 fi
 
 branch=10
-aosp_version=QQ2A.200305.002 # version to use for build id and tag
-aosp_version_real=QQ2A.200305.002 # real base version
-aosp_tag=android-10.0.0_r30
+aosp_version=QQ2A.200405.005 # version to use for build id and tag
+aosp_version_real=QQ2A.200405.005 # real base version
+aosp_tag=android-10.0.0_r33
 
 aosp_forks=(
     device_common
