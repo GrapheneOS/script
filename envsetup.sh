89030eb54830b5fc94b482b83f4c32664f73c47b
Daniel Micay <danielmicay@gmail.com>
2020-04-23 16:11:12 -0400

    remove SCHED_BATCH from envsetup wrapper
    It makes more sense to set this case-by-case to avoid using SCHED_BATCH
    for the interactive shell and interactive tools like the emulator.

--- envsetup.sh
+++ envsetup.sh
@@ -5,7 +5,6 @@ export _JAVA_OPTIONS=-XX:-UsePerfData
 export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date -u +%Y.%m.%d.%H)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
-chrt -b -p 0 $$
 export PATH="$PWD/script/bin:$PATH"
 export BUILD_USERNAME=grapheneos
 export BUILD_HOSTNAME=grapheneos
