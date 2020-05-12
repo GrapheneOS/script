eaec523fb8fbb8854635275cd0e6421d6ce70154
Daniel Micay <danielmicay@gmail.com>
2020-05-12 10:17:54 -0400

    remove downstream python2 PATH workaround

--- envsetup.sh
+++ envsetup.sh
@@ -5,6 +5,5 @@ export _JAVA_OPTIONS=-XX:-UsePerfData
 export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date -u +%Y.%m.%d.%H)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
-export PATH="$PWD/script/bin:$PATH"
 export BUILD_USERNAME=grapheneos
 export BUILD_HOSTNAME=grapheneos
