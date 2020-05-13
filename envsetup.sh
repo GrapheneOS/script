c539d5063af001f035bff9a6710a8bcd3a5e0609
Daniel Micay <danielmicay@gmail.com>
2020-05-12 20:55:01 -0400

    keep build number and datetime consistent

--- envsetup.sh
+++ envsetup.sh
@@ -2,7 +2,9 @@ source build/envsetup.sh
 
 export LANG=en_US.UTF-8
 export _JAVA_OPTIONS=-XX:-UsePerfData
-export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date -u +%Y.%m.%d.%H)
+export BUILD_DATETIME_FROM_FILE=$(cat out/build_date 2>/dev/null || date -u +%s)
+echo "BUILD_DATETIME_FROM_FILE=$BUILD_DATETIME_FROM_FILE"
+export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date -u -d $(BUILD_DATETIME_FROM_FILE) +%Y.%m.%d.%H)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
 export DISPLAY_BUILD_NUMBER=true
 export BUILD_USERNAME=grapheneos
