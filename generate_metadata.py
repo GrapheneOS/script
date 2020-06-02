af1073b4bb84444a3aebcd6ab444998fd1d0d7fa
Daniel Micay <danielmicay@gmail.com>
2020-06-02 13:37:16 -0400

    add channel name to update metadata

--- generate_metadata.py
+++ generate_metadata.py
@@ -12,7 +12,8 @@ zip_path = parser.parse_args().zip
 with ZipFile(zip_path) as f:
     with f.open("META-INF/com/android/metadata") as metadata:
         data = dict(line[:-1].decode().split("=") for line in metadata)
-        with open(path.join(path.dirname(zip_path), data["pre-device"] + "-testing"), "w") as output:
-            build_id = data["post-build"].split("/")[3]
-            incremental = data["post-build"].split("/")[4].split(":")[0]
-            print(incremental, data["post-timestamp"], build_id, file=output)
+        for channel in ("beta", "stable", "testing"):
+            with open(path.join(path.dirname(zip_path), data["pre-device"] + "-" + channel), "w") as output:
+                build_id = data["post-build"].split("/")[3]
+                incremental = data["post-build"].split("/")[4].split(":")[0]
+                print(incremental, data["post-timestamp"], build_id, channel, file=output)
