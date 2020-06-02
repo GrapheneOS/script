d57e38e66cb81feeb73d1a91fb643ccdaf8ac781
Daniel Micay <danielmicay@gmail.com>
2020-06-01 20:18:47 -0400

    June kernel tags

--- manage.sh
+++ manage.sh
@@ -69,9 +69,10 @@ aosp_forks=(
 )
 
 declare -A kernels=(
-    [google_wahoo]=android-10.0.0_r0.60 # May 2019
+    [google_wahoo]=android-10.0.0_r0.64 # June 2019
     # broken tag, using branch temporarily
-    #[google_crosshatch]=android-10.0.0_r0.62 # May 2019
+    [google_crosshatch]=android-10.0.0_r0.66 # June 2019
+    #[google_coral]=android-10.0.0_r0.67 # June 2019
     #[linaro_hikey]=dc721a4ac71d
 )
 
