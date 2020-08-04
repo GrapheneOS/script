32c45fd8b0d73202a596e5488f6e088e8832691d
Daniel Micay <danielmicay@gmail.com>
2020-08-03 23:08:44 -0400

    add missing .txt extension for build date

--- envsetup.sh
+++ envsetup.sh
@@ -3,7 +3,7 @@ source build/envsetup.sh
 
 export LANG=en_US.UTF-8
 export _JAVA_OPTIONS=-XX:-UsePerfData
-export BUILD_DATETIME=$(cat out/build_date 2>/dev/null || date -u +%s)
+export BUILD_DATETIME=$(cat out/build_date.txt 2>/dev/null || date -u +%s)
 echo "BUILD_DATETIME=$BUILD_DATETIME"
 export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date -u -d @$BUILD_DATETIME +%Y.%m.%d.%H)
 echo "BUILD_NUMBER=$BUILD_NUMBER"
