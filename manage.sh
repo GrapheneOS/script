fefe9d208aea854dd05b8f44785c252e689f5f5c
Daniel Micay <danielmicay@gmail.com>
2020-09-09 10:40:27 -0400

    drop DeskClock repository

--- manage.sh
+++ manage.sh
@@ -50,7 +50,6 @@ aosp_forks=(
     platform_packages_apps_Bluetooth
     platform_packages_apps_Camera2
     platform_packages_apps_Contacts
-    platform_packages_apps_DeskClock
     #platform_packages_apps_Gallery2
     platform_packages_apps_Launcher3
     platform_packages_apps_Messaging
