22cc7eb49620ea51262ef599854fcd7198b7ed41
Daniel Micay <danielmicay@gmail.com>
2020-09-16 06:33:47 -0400

    use otatools package

--- release.sh
+++ release.sh
@@ -62,18 +62,20 @@ if [[ $DEVICE != hikey* ]]; then
     fi
 fi
 
-build/tools/releasetools/sign_target_files_apks -o -d "$KEY_DIR" "${VERITY_SWITCHES[@]}" \
+unzip out/target/product/taimen/otatools.zip -d $OUT/otatools || exit 1
+
+$OUT/otatools/releasetools/sign_target_files_apks -o -d "$KEY_DIR" "${VERITY_SWITCHES[@]}" \
     out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/$PREFIX$DEVICE-target_files-$BUILD_NUMBER.zip \
     $OUT/$TARGET_FILES || exit 1
 
 if [[ $DEVICE != hikey* ]]; then
-    build/tools/releasetools/ota_from_target_files -k "$KEY_DIR/releasekey" \
+    $OUT/otatools/releasetools/ota_from_target_files -k "$KEY_DIR/releasekey" \
         "${EXTRA_OTA[@]}" $OUT/$TARGET_FILES \
         $OUT/$DEVICE-ota_update-$BUILD.zip || exit 1
     script/generate_metadata.py $OUT/$DEVICE-ota_update-$BUILD.zip
 fi
 
-build/tools/releasetools/img_from_target_files $OUT/$TARGET_FILES \
+$OUT/otatools/releasetools/img_from_target_files $OUT/$TARGET_FILES \
     $OUT/$DEVICE-img-$BUILD.zip || exit 1
 
 cd $OUT || exit 1
