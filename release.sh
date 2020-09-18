4441ad155d19ba8de9ff9b1f90e56a5f2a70d837
Daniel Micay <danielmicay@gmail.com>
2020-09-18 13:36:26 -0400

    use generic otatools.zip path

--- release.sh
+++ release.sh
@@ -10,7 +10,7 @@ chrt -b -p 0 $$
 [[ -n $BUILD_NUMBER ]] || user_error "expected BUILD_NUMBER in the environment"
 
 PERSISTENT_KEY_DIR=keys/$1
-OUT=out/release-$1-$BUILD_NUMBER
+RELEASE_OUT=out/release-$1-$BUILD_NUMBER
 
 # decrypt keys in advance for improved performance and modern algorithm support
 KEY_DIR=$(mktemp -d /dev/shm/release_keys.XXXXXXXXXX) || exit 1
@@ -22,11 +22,11 @@ OLD_PATH="$PATH"
 export PATH="$PWD/prebuilts/build-tools/linux-x86/bin:$PATH"
 export PATH="$PWD/prebuilts/build-tools/path/linux-x86:$PATH"
 
-rm -rf $OUT || exit 1
-mkdir -p $OUT || exit 1
-unzip out/target/product/taimen/otatools.zip -d $OUT/otatools || exit 1
+rm -rf $RELEASE_OUT || exit 1
+mkdir -p $RELEASE_OUT || exit 1
+unzip $OUT/otatools.zip -d $RELEASE_OUT/otatools || exit 1
 
-source $OUT/otatools/device/common/clear-factory-images-variables.sh || exit 1
+source $RELEASE_OUT/otatools/device/common/clear-factory-images-variables.sh || exit 1
 
 get_radio_image() {
     grep "require version-$1" vendor/$2/vendor-board-info.txt | cut -d '=' -f 2 | tr '[:upper:]' '[:lower:]' || exit 1
@@ -64,21 +64,21 @@ if [[ $DEVICE != hikey* ]]; then
     fi
 fi
 
-$OUT/otatools/releasetools/sign_target_files_apks -o -d "$KEY_DIR" "${VERITY_SWITCHES[@]}" \
+$RELEASE_OUT/otatools/releasetools/sign_target_files_apks -o -d "$KEY_DIR" "${VERITY_SWITCHES[@]}" \
     out/target/product/$DEVICE/obj/PACKAGING/target_files_intermediates/$PREFIX$DEVICE-target_files-$BUILD_NUMBER.zip \
-    $OUT/$TARGET_FILES || exit 1
+    $RELEASE_OUT/$TARGET_FILES || exit 1
 
 if [[ $DEVICE != hikey* ]]; then
-    $OUT/otatools/releasetools/ota_from_target_files -k "$KEY_DIR/releasekey" \
-        "${EXTRA_OTA[@]}" $OUT/$TARGET_FILES \
-        $OUT/$DEVICE-ota_update-$BUILD.zip || exit 1
-    script/generate_metadata.py $OUT/$DEVICE-ota_update-$BUILD.zip || exit 1
+    $RELEASE_OUT/otatools/releasetools/ota_from_target_files -k "$KEY_DIR/releasekey" \
+        "${EXTRA_OTA[@]}" $RELEASE_OUT/$TARGET_FILES \
+        $RELEASE_OUT/$DEVICE-ota_update-$BUILD.zip || exit 1
+    script/generate_metadata.py $RELEASE_OUT/$DEVICE-ota_update-$BUILD.zip || exit 1
 fi
 
-$OUT/otatools/releasetools/img_from_target_files $OUT/$TARGET_FILES \
-    $OUT/$DEVICE-img-$BUILD.zip || exit 1
+$RELEASE_OUT/otatools/releasetools/img_from_target_files $RELEASE_OUT/$TARGET_FILES \
+    $RELEASE_OUT/$DEVICE-img-$BUILD.zip || exit 1
 
-cd $OUT || exit 1
+cd $RELEASE_OUT || exit 1
 
 if [[ $DEVICE == hikey* ]]; then
     source otatools/device/linaro/hikey/factory-images/generate-factory-images-$DEVICE.sh || exit 1
@@ -90,5 +90,5 @@ cd ../..
 
 if [[ -f "$KEY_DIR/factory.sec" ]]; then
     export PATH="$OLD_PATH"
-    script/signify_prehash.sh "$KEY_DIR/factory.sec" $OUT/$DEVICE-factory-$BUILD_NUMBER.zip || exit 1
+    script/signify_prehash.sh "$KEY_DIR/factory.sec" $RELEASE_OUT/$DEVICE-factory-$BUILD_NUMBER.zip || exit 1
 fi
