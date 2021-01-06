a4df70711a34c7ae58a3d9e323b0ba738b5e6f49
Daniel Micay <danielmicay@gmail.com>
2021-01-06 07:00:13 -0500

    use 11-wahoo branch

--- manage.sh
+++ manage.sh
@@ -126,6 +126,7 @@ for repo in "${aosp_forks[@]}"; do
     cd $repo
 
     git checkout $branch
+    git checkout 11-wahoo || true
 
     if [[ -n $DELETE_TAG ]]; then
         git tag -d $DELETE_TAG
@@ -167,6 +168,7 @@ for kernel in ${!kernels[@]}; do
 
     cd kernel_$kernel
     git checkout $branch
+    git checkout 11-wahoo || true
 
     if [[ -n $DELETE_TAG ]]; then
         git tag -d $DELETE_TAG
@@ -199,6 +201,7 @@ for repo in ${independent[@]}; do
 
     cd $repo
     git checkout $branch
+    git checkout 11-wahoo || true
 
     if [[ -n $DELETE_TAG ]]; then
         git tag -d $DELETE_TAG
