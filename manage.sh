c7075d4ec0093ebb0b2d08c9c6dda8abc7acf4bb
Daniel Micay <danielmicay@gmail.com>
2021-05-02 14:48:12 -0400

    add platform_packages_apps_ThemePicker

--- manage.sh
+++ manage.sh
@@ -58,6 +58,7 @@ aosp_forks=(
     platform_packages_apps_QuickSearchBox
     platform_packages_apps_Settings
     platform_packages_apps_SettingsIntelligence
+    platform_packages_apps_ThemePicker
     platform_packages_apps_WallpaperPicker2
     platform_packages_inputmethods_LatinIME
     platform_packages_modules_NetworkStack
