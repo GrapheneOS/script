eac9d292ef4482b19a088f94ad0179c19ad7d6bc
Daniel Micay <danielmicay@gmail.com>
2021-06-07 19:10:51 -0400

    June 2021 kernel tags

--- manage.sh
+++ manage.sh
@@ -73,20 +73,20 @@ aosp_forks=(
 )
 
 declare -A kernels=(
-    [google_crosshatch]=android-11.0.0_r0.77 # May 2021
-    [google_crosshatch_drivers_staging_qcacld-3.0]=android-11.0.0_r0.77 # May 2021
-    [google_crosshatch_techpack_audio]=android-11.0.0_r0.77 # May 2021
-    [google_coral]=android-11.0.0_r0.79 # May 2021
-    [google_coral_drivers_staging_qcacld-3.0]=android-11.0.0_r0.79 # May 2021
-    [google_coral_techpack_audio]=android-11.0.0_r0.79 # May 2021
-    [google_sunfish]=android-11.0.0_r0.80 # May 2021
-    [google_sunfish_drivers_staging_qcacld-3.0]=android-11.0.0_r0.80 # May 2021
-    [google_sunfish_techpack_audio]=android-11.0.0_r0.80 # May 2021
-    [google_redbull]=android-11.0.0_r0.81 # May 2021
-    [google_redbull_drivers_staging_qca-wifi-host-cmn]=android-11.0.0_r0.81 # May 2021
-    [google_redbull_drivers_staging_qcacld-3.0]=android-11.0.0_r0.81 # May 2021
-    [google_redbull_techpack_audio]=android-11.0.0_r0.81 # May 2021
-    [google_redbull_arch_arm64_boot_dts_vendor]=android-11.0.0_r0.81 # May 2021
+    [google_crosshatch]=android-11.0.0_r0.82 # June 2021
+    [google_crosshatch_drivers_staging_qcacld-3.0]=android-11.0.0_r0.82 # June 2021
+    [google_crosshatch_techpack_audio]=android-11.0.0_r0.82 # June 2021
+    [google_coral]=android-11.0.0_r0.84 # June 2021
+    [google_coral_drivers_staging_qcacld-3.0]=android-11.0.0_r0.84 # June 2021
+    [google_coral_techpack_audio]=android-11.0.0_r0.84 # June 2021
+    [google_sunfish]=android-11.0.0_r0.85 # June 2021
+    [google_sunfish_drivers_staging_qcacld-3.0]=android-11.0.0_r0.85 # June 2021
+    [google_sunfish_techpack_audio]=android-11.0.0_r0.85 # June 2021
+    [google_redbull]=android-11.0.0_r0.86 # June 2021
+    [google_redbull_drivers_staging_qca-wifi-host-cmn]=android-11.0.0_r0.86 # June 2021
+    [google_redbull_drivers_staging_qcacld-3.0]=android-11.0.0_r0.86 # June 2021
+    [google_redbull_techpack_audio]=android-11.0.0_r0.86 # June 2021
+    [google_redbull_arch_arm64_boot_dts_vendor]=android-11.0.0_r0.86 # June 2021
 )
 
 independent=(
