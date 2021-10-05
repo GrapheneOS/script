f3c9470885a036ac19e9224b496d8088b1d4f82e
Daniel Micay <danielmicay@gmail.com>
2021-10-04 20:26:51 -0400

    stop hard-wiring branch in fork.sh

--- fork.sh
+++ fork.sh
@@ -16,10 +16,10 @@ upstream="https://android.googlesource.com/$repo"
 git clone $upstream -b $aosp_tag
 mv $(basename $repo) $local_repo
 cd $local_repo
-git checkout -b 11
+git checkout -b $branch
 git remote add upstream $upstream
 git fetch upstream --tags
 git remote rm origin
 hub create GrapheneOS/$local_repo -h https://grapheneos.org/
-git push -u origin 11
+git push -u origin $branch
 xdg-open https://github.com/GrapheneOS/$local_repo
