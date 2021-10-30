7e13fc640c0b649ae78ce442ae2cde133c4f35b1
Daniel Micay <danielmicay@gmail.com>
2021-10-30 10:51:46 -0400

    set up temporary 6th generation Pixel branch

--- common.sh
+++ common.sh
@@ -1,6 +1,8 @@
-branch=12
-aosp_version=SQ1A.220205.002
-aosp_tag=android-12.0.0_r28
+branch=12-raviole
+branch_base=12
+aosp_version=SQ1D.220205.003
+aosp_tag=android-12.0.0_r29
+aosp_tag_base=android-12.0.0_r28
 
 user_error() {
     echo $1 >&2
