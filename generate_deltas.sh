e6d53af41294ca9610e40671f5cb09d5a4c8a369
Daniel Micay <danielmicay@gmail.com>
2021-11-02 06:23:52 -0400

    drop crosshatch/blueline from generate_deltas.sh

--- generate_deltas.sh
+++ generate_deltas.sh
@@ -19,4 +19,4 @@ rm -rf delta-generation
 mkdir delta-generation
 export TMPDIR="$PWD/delta-generation"
 
-parallel --use-cores-instead-of-threads -q script/generate_delta.sh ::: barbet redfin bramble sunfish coral flame bonito sargo crosshatch blueline ::: $@ ::: $SOURCE
+parallel --use-cores-instead-of-threads -q script/generate_delta.sh ::: barbet redfin bramble sunfish coral flame bonito sargo ::: $@ ::: $SOURCE
