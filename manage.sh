a229a3aaf835b6538f40b6e3addd0fca634fd958
Daniel Micay <danielmicay@gmail.com>
2021-11-02 06:38:15 -0400

    crosshatch/blueline ESR branch

--- manage.sh
+++ manage.sh
@@ -136,8 +136,11 @@ for repo in "${aosp_forks[@]}"; do
     echo -e "\n>>> $(tput setaf 3)Handling $repo$(tput sgr0)"
 
     cd $repo
-
-    git checkout $branch
+    if [[ $repo == @(platform_manifest|platform_build) ]]; then
+        git checkout 12-crosshatch
+    else
+        git checkout $branch
+    fi
 
     if [[ -n $DELETE_TAG ]]; then
         git tag -d $DELETE_TAG
@@ -161,7 +164,7 @@ for repo in "${aosp_forks[@]}"; do
         fi
 
         if [[ $repo == platform_manifest ]]; then
-            git checkout $branch
+            git checkout 12-crosshatch
             git branch -D tmp
         fi
     else
@@ -210,7 +213,11 @@ for repo in ${independent[@]}; do
     echo -e "\n>>> $(tput setaf 3)Handling $repo$(tput sgr0)"
 
     cd $repo
-    git checkout $branch
+    if [[ $repo == script ]]; then
+        git checkout 12-crosshatch
+    else
+        git checkout $branch
+    fi
 
     if [[ -n $DELETE_TAG ]]; then
         git tag -d $DELETE_TAG
