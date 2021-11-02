fa4a5856dfd0bc4e7298801a72dfcfa8e89f8aa8
Daniel Micay <danielmicay@gmail.com>
2021-11-02 06:38:15 -0400

    crosshatch/blueline ESR branch

--- manage.sh
+++ manage.sh
@@ -150,8 +150,11 @@ for repo in "${aosp_forks[@]}"; do
     echo -e "\n>>> $(tput setaf 3)Handling $repo$(tput sgr0)"
 
     cd $repo
-
-    git checkout $branch
+    if [[ $repo == @(platform_manifest|platform_build) ]]; then
+        git checkout 12.1-crosshatch
+    else
+        git checkout $branch
+    fi
 
     if [[ -n $DELETE_TAG ]]; then
         git tag -d $DELETE_TAG
@@ -163,6 +166,7 @@ for repo in "${aosp_forks[@]}"; do
     if [[ -n $build_number ]]; then
         if [[ $repo == platform_manifest ]]; then
             git checkout -B tmp
+            sed -i s%refs/heads/12.1-crosshatch%refs/tags/$aosp_version.$build_number% default.xml
             sed -i s%refs/heads/$branch%refs/tags/$aosp_version.$build_number% default.xml
             git commit default.xml -m $aosp_version.$build_number
         fi
@@ -175,7 +179,7 @@ for repo in "${aosp_forks[@]}"; do
         fi
 
         if [[ $repo == platform_manifest ]]; then
-            git checkout $branch
+            git checkout 12.1-crosshatch
             git branch -D tmp
         fi
     else
@@ -224,7 +228,11 @@ for repo in ${independent[@]}; do
     echo -e "\n>>> $(tput setaf 3)Handling $repo$(tput sgr0)"
 
     cd $repo
-    git checkout $branch
+    if [[ $repo == script ]]; then
+        git checkout 12.1-crosshatch
+    else
+        git checkout $branch
+    fi
 
     if [[ -n $DELETE_TAG ]]; then
         git tag -d $DELETE_TAG
