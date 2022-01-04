2163d66e664052c2ed9e4849842bd909893c7248
Daniel Micay <danielmicay@gmail.com>
2022-01-04 17:36:37 -0500

    January kernel tags

--- manage.sh
+++ manage.sh
@@ -72,28 +72,28 @@ aosp_forks=(
 )
 
 declare -A kernels=(
-    # December 2021
-    [google_crosshatch]=android-12.0.0_r0.21
-    [google_crosshatch_drivers_staging_qcacld-3.0]=android-12.0.0_r0.21
-    [google_crosshatch_techpack_audio]=android-12.0.0_r0.21
+    # January 2021
+    [google_crosshatch]=android-12.0.0_r0.31
+    [google_crosshatch_drivers_staging_qcacld-3.0]=android-12.0.0_r0.31
+    [google_crosshatch_techpack_audio]=android-12.0.0_r0.31
 
     # December 2021
-    [google_coral]=android-12.0.0_r0.22
-    [google_coral_drivers_input_touchscreen_fts_touch_s5]=android-12.0.0_r0.22
-    [google_coral_drivers_staging_qcacld-3.0]=android-12.0.0_r0.22
-    [google_coral_techpack_audio]=android-12.0.0_r0.22
+    [google_coral]=android-12.0.0_r0.32
+    [google_coral_drivers_input_touchscreen_fts_touch_s5]=android-12.0.0_r0.32
+    [google_coral_drivers_staging_qcacld-3.0]=android-12.0.0_r0.32
+    [google_coral_techpack_audio]=android-12.0.0_r0.32
 
     # December 2021
-    [google_redbull]=android-12.0.0_r0.24
-    [google_redbull_drivers_staging_qcacld-3.0]=android-12.0.0_r0.24
-    [google_redbull_techpack_audio]=android-12.0.0_r0.24
-    [google_redbull_arch_arm64_boot_dts_vendor]=android-12.0.0_r0.24
+    [google_redbull]=android-12.0.0_r0.34
+    [google_redbull_drivers_staging_qcacld-3.0]=android-12.0.0_r0.34
+    [google_redbull_techpack_audio]=android-12.0.0_r0.34
+    [google_redbull_arch_arm64_boot_dts_vendor]=android-12.0.0_r0.34
 
     # December 2021
-    [google_barbet]=android-12.0.0_r0.25
-    [google_barbet_drivers_staging_qcacld-3.0]=android-12.0.0_r0.25
-    [google_barbet_techpack_audio]=android-12.0.0_r0.25
-    [google_barbet_arch_arm64_boot_dts_vendor]=android-12.0.0_r0.25
+    [google_barbet]=android-12.0.0_r0.35
+    [google_barbet_drivers_staging_qcacld-3.0]=android-12.0.0_r0.35
+    [google_barbet_techpack_audio]=android-12.0.0_r0.35
+    [google_barbet_arch_arm64_boot_dts_vendor]=android-12.0.0_r0.35
 
     # December 2021
     [common_5.10]=android-12.0.0_r0.26
