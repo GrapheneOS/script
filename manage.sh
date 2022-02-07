59674558326e224f5adb72cfd581e6def718a9b5
Daniel Micay <danielmicay@gmail.com>
2022-02-07 18:27:23 -0500

    update kernel tags

--- manage.sh
+++ manage.sh
@@ -74,36 +74,36 @@ aosp_forks=(
 )
 
 declare -A kernels=(
-    # January 2022
-    [google_crosshatch]=android-12.0.0_r0.31
-    [google_crosshatch_drivers_staging_qcacld-3.0]=android-12.0.0_r0.31
-    [google_crosshatch_techpack_audio]=android-12.0.0_r0.31
+    # February 2022
+    [google_crosshatch]=android-12.0.0_r0.37
+    [google_crosshatch_drivers_staging_qcacld-3.0]=android-12.0.0_r0.37
+    [google_crosshatch_techpack_audio]=android-12.0.0_r0.37
 
-    # January 2022
-    [google_coral]=android-12.0.0_r0.32
-    [google_coral_drivers_input_touchscreen_fts_touch_s5]=android-12.0.0_r0.32
-    [google_coral_drivers_staging_qcacld-3.0]=android-12.0.0_r0.32
-    [google_coral_techpack_audio]=android-12.0.0_r0.32
+    # February 2022
+    [google_coral]=android-12.0.0_r0.38
+    [google_coral_drivers_input_touchscreen_fts_touch_s5]=android-12.0.0_r0.38
+    [google_coral_drivers_staging_qcacld-3.0]=android-12.0.0_r0.38
+    [google_coral_techpack_audio]=android-12.0.0_r0.38
 
-    # January 2022
-    [google_redbull]=android-12.0.0_r0.34
-    [google_redbull_drivers_staging_qcacld-3.0]=android-12.0.0_r0.34
-    [google_redbull_techpack_audio]=android-12.0.0_r0.34
-    [google_redbull_arch_arm64_boot_dts_vendor]=android-12.0.0_r0.34
+    # February 2022
+    [google_redbull]=android-12.0.0_r0.40
+    [google_redbull_drivers_staging_qcacld-3.0]=android-12.0.0_r0.40
+    [google_redbull_techpack_audio]=android-12.0.0_r0.40
+    [google_redbull_arch_arm64_boot_dts_vendor]=android-12.0.0_r0.40
 
-    # January 2022
-    [google_barbet]=android-12.0.0_r0.35
-    [google_barbet_drivers_staging_qcacld-3.0]=android-12.0.0_r0.35
-    [google_barbet_techpack_audio]=android-12.0.0_r0.35
-    [google_barbet_arch_arm64_boot_dts_vendor]=android-12.0.0_r0.35
+    # February 2022
+    [google_barbet]=android-12.0.0_r0.41
+    [google_barbet_drivers_staging_qcacld-3.0]=android-12.0.0_r0.41
+    [google_barbet_techpack_audio]=android-12.0.0_r0.41
+    [google_barbet_arch_arm64_boot_dts_vendor]=android-12.0.0_r0.41
 
-    # January 2022
-    [common_5.10]=android-12.0.0_r0.36
+    # February 2022
+    [common_5.10]=android-12.0.0_r0.42
 
-    # January 2022
-    [google_raviole]=android-12.0.0_r0.36
-    [google-modules_wlan_bcmdhd_bcm43752]=android-12.0.0_r0.36
-    [google-modules_wlan_bcmdhd_bcm4389]=android-12.0.0_r0.36
+    # February 2022
+    [google_raviole]=android-12.0.0_r0.42
+    [google-modules_wlan_bcmdhd_bcm43752]=android-12.0.0_r0.42
+    [google-modules_wlan_bcmdhd_bcm4389]=android-12.0.0_r0.42
 )
 
 independent=(
