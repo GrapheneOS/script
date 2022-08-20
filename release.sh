7fa87d132aef1cabacbccf3ce529bd49fe389010
Daniel Micay <danielmicay@gmail.com>
2022-08-20 13:00:27 -0400

    add new apex modules to release signing script

--- release.sh
+++ release.sh
@@ -82,6 +82,8 @@ sign_target_files_apks -o -d "$KEY_DIR" --avb_vbmeta_key "$KEY_DIR/avb.pem" --av
     --extra_apks WifiDialog.apk="$KEY_DIR/releasekey" \
     --extra_apks com.android.adbd.apex="$KEY_DIR/releasekey" \
     --extra_apex_payload_key com.android.adbd.apex="$KEY_DIR/avb.pem" \
+    --extra_apks com.android.adservices.apex="$KEY_DIR/releasekey" \
+    --extra_apex_payload_key com.android.adservices.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.apex.cts.shim.apex="$KEY_DIR/releasekey" \
     --extra_apex_payload_key com.android.apex.cts.shim.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.appsearch.apex="$KEY_DIR/releasekey" \
@@ -90,8 +92,12 @@ sign_target_files_apks -o -d "$KEY_DIR" --avb_vbmeta_key "$KEY_DIR/avb.pem" --av
     --extra_apex_payload_key com.android.art.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.art.debug.apex="$KEY_DIR/releasekey" \
     --extra_apex_payload_key com.android.art.debug.apex="$KEY_DIR/avb.pem" \
+    --extra_apks com.android.btservices.apex="$KEY_DIR/bluetooth" \
+    --extra_apex_payload_key com.android.btservices.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.cellbroadcast.apex="$KEY_DIR/releasekey" \
     --extra_apex_payload_key com.android.cellbroadcast.apex="$KEY_DIR/avb.pem" \
+    --extra_apks com.android.compos.apex="$KEY_DIR/releasekey" \
+    --extra_apex_payload_key com.android.compos.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.conscrypt.apex="$KEY_DIR/releasekey" \
     --extra_apex_payload_key com.android.conscrypt.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.extservices.apex="$KEY_DIR/releasekey" \
@@ -108,6 +114,8 @@ sign_target_files_apks -o -d "$KEY_DIR" --avb_vbmeta_key "$KEY_DIR/avb.pem" --av
     --extra_apex_payload_key com.android.mediaprovider.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.neuralnetworks.apex="$KEY_DIR/releasekey" \
     --extra_apex_payload_key com.android.neuralnetworks.apex="$KEY_DIR/avb.pem" \
+    --extra_apks com.android.ondevicepersonalization.apex="$KEY_DIR/releasekey" \
+    --extra_apex_payload_key com.android.ondevicepersonalization.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.os.statsd.apex="$KEY_DIR/releasekey" \
     --extra_apex_payload_key com.android.os.statsd.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.permission.apex="$KEY_DIR/releasekey" \
@@ -124,6 +132,10 @@ sign_target_files_apks -o -d "$KEY_DIR" --avb_vbmeta_key "$KEY_DIR/avb.pem" --av
     --extra_apex_payload_key com.android.tethering.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.tzdata.apex="$KEY_DIR/releasekey" \
     --extra_apex_payload_key com.android.tzdata.apex="$KEY_DIR/avb.pem" \
+    --extra_apks com.android.uwb.apex="$KEY_DIR/releasekey" \
+    --extra_apex_payload_key com.android.uwb.apex="$KEY_DIR/avb.pem" \
+    --extra_apks com.android.virt.apex="$KEY_DIR/releasekey" \
+    --extra_apex_payload_key com.android.virt.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.vndk.current.apex="$KEY_DIR/releasekey" \
     --extra_apex_payload_key com.android.vndk.current.apex="$KEY_DIR/avb.pem" \
     --extra_apks com.android.wifi.apex="$KEY_DIR/releasekey" \
