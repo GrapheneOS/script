67b3bc7056422a75f2eb462e8b392c169b7aeb72
Daniel Micay <danielmicay@gmail.com>
2022-09-07 19:38:27 -0400

    use legacy-releases directory

--- generate_delta.sh
+++ generate_delta.sh
@@ -21,9 +21,9 @@ script/decrypt_keys.sh "$KEY_DIR"
 
 export PATH="$PWD/prebuilts/build-tools/linux-x86/bin:$PATH"
 export PATH="$PWD/prebuilts/build-tools/path/linux-x86:$PATH"
-export PATH="$PWD/releases/$NEW/release-$DEVICE-$NEW/bin:$PATH"
+export PATH="$PWD/legacy-releases/$NEW/release-$DEVICE-$NEW/bin:$PATH"
 
-cd "releases/$NEW"
+cd "legacy-releases/$NEW"
 
 ota_from_target_files "${EXTRA_OTA[@]}" -k "$KEY_DIR/releasekey" \
     -i ../$OLD/release-$DEVICE-$OLD/$DEVICE-target_files-$OLD.zip \
