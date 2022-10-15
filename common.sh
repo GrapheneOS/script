74b756f2b65a9cfa807c013ee20eff13a589605e
Daniel Micay <danielmicay@gmail.com>
2022-10-14 21:17:30 -0400

    temporary Pixel 7 / Pixel 7 Pro device branch

--- common.sh
+++ common.sh
@@ -1,6 +1,8 @@
-branch=13
-aosp_version=TP1A.221005.003
-aosp_tag=android-13.0.0_r8
+branch_base=13
+branch=13-pantah
+aosp_version=TD1A.220804.031
+aosp_tag=android-13.0.0_r11
+aosp_tag_base=android-13.0.0_r8
 
 user_error() {
     echo $1 >&2
