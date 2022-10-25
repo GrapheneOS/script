b59ed811d3a090fe3711db555251e032caab31c1
Daniel Micay <danielmicay@gmail.com>
2022-10-25 15:26:25 -0400

    dd platform_external_crosvm

--- manage.sh
+++ manage.sh
@@ -39,6 +39,7 @@ aosp_forks=(
     platform_external_android-nn-driver
     platform_external_armnn
     platform_external_conscrypt
+    platform_external_crosvm
     platform_frameworks_base
     platform_frameworks_ex
     platform_frameworks_libs_modules-utils
