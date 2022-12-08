a428888e9378efacc4dc229ef2ac7e63905ac562
Daniel Micay <danielmicay@gmail.com>
2022-12-08 07:17:25 -0500

    coral/flame extended support branch

--- manage.sh
+++ manage.sh
@@ -202,7 +202,11 @@ for repo in "${aosp_forks[@]}"; do
     echo -e "\n>>> $(tput setaf 3)Handling $repo$(tput sgr0)"
 
     cd $repo
-    git checkout $branch
+    if [[ $repo == @(platform_frameworks_native|platform_manifest) ]]; then
+        git checkout 13-coral
+    else
+        git checkout $branch
+    fi
 
     if [[ $action == delete ]]; then
         git tag -d $tag_name || true
@@ -259,7 +263,11 @@ for repo in ${independent[@]}; do
     echo -e "\n>>> $(tput setaf 3)Handling $repo$(tput sgr0)"
 
     cd $repo
-    git checkout $branch
+    if [[ $repo == script ]]; then
+        git checkout 13-coral
+    else
+        git checkout $branch
+    fi
 
     if [[ $action == delete ]]; then
         git tag -d $tag_name || true
