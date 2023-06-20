f75e64cb2ce7cd98f0db06fac5a1221f701ad5cc
Daniel Micay <daniel.micay@grapheneos.org>
2023-06-19 21:06:04 -0400

    Pixel Tablet (tangorpro) device branch

--- common.sh
+++ common.sh
@@ -1,6 +1,8 @@
-readonly branch=13
-readonly aosp_tag_old=android-13.0.0_r67
-readonly aosp_tag=android-13.0.0_r67
+readonly base_branch=13
+readonly branch=13-tangorpro
+readonly aosp_tag_old=android-13.0.0_r71
+readonly aosp_tag=android-13.0.0_r71
+readonly aosp_base_tag=android-13.0.0_r67
 
 user_error() {
     echo $1 >&2
