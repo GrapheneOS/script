fe19d07b988d6ca39bf03c2ba3826e2947585476
Daniel Micay <daniel.micay@grapheneos.org>
2023-08-07 16:37:27 -0400

    update to TQ3A.230805.001

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 readonly branch=13
-readonly aosp_tag_old=android-13.0.0_r61
-readonly aosp_tag=android-13.0.0_r61
+readonly aosp_tag_old=android-13.0.0_r67
+readonly aosp_tag=android-13.0.0_r67
 
 user_error() {
     echo $1 >&2
