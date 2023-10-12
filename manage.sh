505eed3753da7cbf92c9dfbab51378ce4a2e72bf
Daniel Micay <daniel.micay@grapheneos.org>
2023-10-12 15:34:13 -0400

    drop platform_packages_apps_SettingsIntelligence

--- manage.sh
+++ manage.sh
@@ -74,7 +74,6 @@ readonly aosp_forks=(
     platform_packages_apps_Nfc
     platform_packages_apps_QuickSearchBox
     platform_packages_apps_Settings
-    platform_packages_apps_SettingsIntelligence
     platform_packages_apps_ThemePicker
     platform_packages_apps_WallpaperPicker2
     platform_packages_inputmethods_LatinIME
