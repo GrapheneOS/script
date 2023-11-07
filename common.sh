101545f41954a30e3c256f7f091c6bfb2e378b2d
Daniel Micay <daniel.micay@grapheneos.org>
2023-11-06 21:36:34 -0500

    update to UP1A.231105.003

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 readonly branch=14
-readonly aosp_tag_old=android-14.0.0_r1
-readonly aosp_tag=android-14.0.0_r1
+readonly aosp_tag_old=android-14.0.0_r14
+readonly aosp_tag=android-14.0.0_r14
 
 user_error() {
     echo $1 >&2
