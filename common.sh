f63e9c70fef1c651f752706b0ed35fef6372b6a0
Daniel Micay <daniel.micay@grapheneos.org>
2023-11-07 01:46:18 -0500

    Pixel 8 and Pixel 8 Pro device branch

--- common.sh
+++ common.sh
@@ -1,6 +1,8 @@
-readonly branch=14
-readonly aosp_tag_old=android-14.0.0_r14
-readonly aosp_tag=android-14.0.0_r14
+readonly base_branch=14
+readonly branch=14-shusky
+readonly aosp_tag_old=android-14.0.0_r12
+readonly aosp_tag=android-14.0.0_r12
+readonly aosp_base_tag=android-14.0.0_r14
 
 user_error() {
     echo $1 >&2
