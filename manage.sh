f23d93716b3f9d5f0ee63a72a22cad7a9c65b273
Daniel Micay <daniel.micay@grapheneos.org>
2023-12-13 00:08:45 -0500

    redfin/bramble extended support branch

--- manage.sh
+++ manage.sh
@@ -210,7 +210,11 @@ for repo in "${aosp_forks[@]}"; do
     echo -e "\n>>> $(tput setaf 3)Handling $repo$(tput sgr0)"
 
     cd $repo
-    git checkout $branch
+    if [[ $repo == platform_manifest ]]; then
+        git checkout 14-redfin
+    else
+        git checkout $branch
+    fi
 
     if [[ $action == delete ]]; then
         git tag -d $tag_name || true
@@ -273,7 +277,11 @@ for repo in ${independent[@]}; do
     echo -e "\n>>> $(tput setaf 3)Handling $repo$(tput sgr0)"
 
     cd $repo
-    git checkout $branch
+    if [[ $repo == script ]]; then
+        git checkout 14-redfin
+    else
+        git checkout $branch
+    fi
 
     if [[ $action == delete ]]; then
         git tag -d $tag_name || true
