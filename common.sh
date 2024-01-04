b9d62c93ad5e36344858b22f55dc0703ccd98dd8
Daniel Micay <daniel.micay@grapheneos.org>
2024-01-03 19:33:04 -0500

    update to UQ1A.240105.004

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 readonly branch=14
-readonly aosp_tag_old=android-14.0.0_r17
-readonly aosp_tag=android-14.0.0_r17
+readonly aosp_tag_old=android-14.0.0_r20
+readonly aosp_tag=android-14.0.0_r20
 
 user_error() {
     echo $1 >&2
