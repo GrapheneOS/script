cb4c84795b74d8f62f754be491671440ebc38061
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-02-20 20:58:14 +0200

    replace SetupWizard with SetupWizard2

--- manage.sh
+++ manage.sh
@@ -198,7 +198,7 @@ readonly independent=(
     platform_packages_apps_ExactCalculator
     platform_packages_apps_GmsCompat
     platform_packages_apps_LogViewer
-    platform_packages_apps_SetupWizard
+    platform_packages_apps_SetupWizard2
     platform_packages_apps_Updater
     script
     vendor_state
