a45e6711e50eab411b23a6686092974f0b302b3f
Daniel Micay <daniel.micay@grapheneos.org>
2024-02-26 15:59:18 -0500

    add platform_hardware_interfaces

--- manage.sh
+++ manage.sh
@@ -56,6 +56,7 @@ readonly aosp_forks=(
     platform_frameworks_native
     platform_frameworks_opt_net_wifi
     platform_frameworks_opt_telephony
+    platform_hardware_interfaces
     platform_hardware_google_graphics_common
     platform_hardware_google_pixel
     platform_hardware_google_pixel-sepolicy
