96b99dccb8143e3dab9b09d8562a85d8af33f994
Daniel Micay <daniel.micay@grapheneos.org>
2025-02-06 06:21:34 -0500

    Revert "add platform_frameworks_av"
    This reverts commit b604628ba84de049f6d2fdbe998e87c1e45a26e4.

--- manage.sh
+++ manage.sh
@@ -49,7 +49,6 @@ readonly aosp_forks=(
     platform_external_boringssl
     platform_external_conscrypt
     platform_external_selinux
-    platform_frameworks_av
     platform_frameworks_base
     platform_frameworks_libs_systemui
     platform_frameworks_native
