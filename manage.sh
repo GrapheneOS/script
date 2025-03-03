75ce82f2a6066a1b0ebc04463d7766cd36ae21b9
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-03 21:03:51 +0200

    add platform_external_dng_sdk

--- manage.sh
+++ manage.sh
@@ -48,6 +48,7 @@ readonly aosp_forks=(
     platform_development
     platform_external_boringssl
     platform_external_conscrypt
+    platform_external_dng_sdk
     platform_external_selinux
     platform_frameworks_base
     platform_frameworks_libs_systemui
