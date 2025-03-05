fb6c6f52076c8746255c36153d670a3e04a39865
Daniel Micay <daniel.micay@grapheneos.org>
2025-03-05 07:27:09 -0500

    Android 15 QPR2 tags

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 readonly branch=15-qpr2
-readonly aosp_tag_old=android-15.0.0_r14
-readonly aosp_tag=android-15.0.0_r14
+readonly aosp_tag_old=android-15.0.0_r23
+readonly aosp_tag=android-15.0.0_r23
 
 user_error() {
     echo $1 >&2
