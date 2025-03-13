5314b1d80200e9d41db31587387d9da69f2553f3
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-03-13 18:52:44 +0200

    add platform_packages_modules_AppSearch

--- manage.sh
+++ manage.sh
@@ -75,6 +75,7 @@ readonly aosp_forks=(
     platform_packages_apps_WallpaperPicker2
     platform_packages_inputmethods_LatinIME
     platform_packages_modules_adb
+    platform_packages_modules_AppSearch
     platform_packages_modules_Bluetooth
     platform_packages_modules_common
     platform_packages_modules_ConfigInfrastructure
