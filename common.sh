328b0b043f6f9032c500631e482df8270090a2de
Daniel Micay <daniel.micay@grapheneos.org>
2025-04-13 02:41:32 -0400

    Pixel 9a device branch

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
-readonly branch=15-qpr2
-readonly aosp_tag_old=android-15.0.0_r30
-readonly aosp_tag=android-15.0.0_r30
+readonly branch=15-qpr1-tegu
+readonly aosp_tag_old=android-15.0.0_r25
+readonly aosp_tag=android-15.0.0_r25
 
 user_error() {
     echo $1 >&2
--- manage.sh
+++ manage.sh
@@ -34,6 +34,7 @@ readonly aosp_forks=(
     device_google_raviole
     device_google_shusky
     device_google_tangorpro
+    device_google_tegu
     device_google_zuma
     device_google_zuma-sepolicy
     device_google_zumapro
@@ -106,97 +107,55 @@ readonly aosp_forks=(
 
 readonly kernels=(
     kernel_build
-    kernel_devices_google_akita
-    kernel_devices_google_bluejay
-    kernel_devices_google_caimito
-    kernel_devices_google_comet
-    kernel_devices_google_felix
-    kernel_devices_google_lynx
-    kernel_devices_google_pantah
-    kernel_devices_google_raviole
-    kernel_devices_google_shusky
-    kernel_devices_google_tangorpro
+    kernel_devices_google_tegu
     kernel_google-modules_amplifiers
     kernel_google-modules_bms
     kernel_google-modules_edgetpu_rio
-    kernel_google-modules_gxp_gs201
     kernel_google-modules_gxp_zuma
     kernel_google-modules_power_reset
     kernel_google-modules_soc_gs
     kernel_google-modules_wlan_bcmdhd_bcm4383
-    kernel_google-modules_wlan_bcmdhd_bcm4389
-    kernel_google-modules_wlan_bcmdhd_bcm4390
-    kernel_google-modules_wlan_bcmdhd_bcm4398
-    kernel_google-modules_wlan_syna_dhd43752p
 )
 
 declare -Ar kernel_tags_old=(
     # April 2025
-    [kernel_build]=android-15.0.0_r0.79
-    [kernel_devices_google_akita]=android-15.0.0_r0.79
-    [kernel_devices_google_bluejay]=android-15.0.0_r0.79
-    [kernel_devices_google_caimito]=android-15.0.0_r0.79
-    [kernel_devices_google_comet]=android-15.0.0_r0.79
-    [kernel_devices_google_felix]=android-15.0.0_r0.79
-    [kernel_devices_google_lynx]=android-15.0.0_r0.79
-    [kernel_devices_google_pantah]=android-15.0.0_r0.79
-    [kernel_devices_google_raviole]=android-15.0.0_r0.79
-    [kernel_devices_google_shusky]=android-15.0.0_r0.79
-    [kernel_devices_google_tangorpro]=android-15.0.0_r0.79
-    [kernel_google-modules_amplifiers]=android-15.0.0_r0.79
-    [kernel_google-modules_bms]=android-15.0.0_r0.79
-    [kernel_google-modules_edgetpu_rio]=android-15.0.0_r0.79
-    [kernel_google-modules_gxp_gs201]=android-15.0.0_r0.79
-    [kernel_google-modules_gxp_zuma]=android-15.0.0_r0.79
-    [kernel_google-modules_power_reset]=android-15.0.0_r0.79
-    [kernel_google-modules_soc_gs]=android-15.0.0_r0.79
-    [kernel_google-modules_wlan_bcmdhd_bcm4383]=android-15.0.0_r0.79
-    [kernel_google-modules_wlan_bcmdhd_bcm4389]=android-15.0.0_r0.79
-    [kernel_google-modules_wlan_bcmdhd_bcm4390]=android-15.0.0_r0.79
-    [kernel_google-modules_wlan_bcmdhd_bcm4398]=android-15.0.0_r0.79
-    [kernel_google-modules_wlan_syna_dhd43752p]=android-15.0.0_r0.79
+    [kernel_build]=android-15.0.0_r0.81
+    [kernel_devices_google_tegu]=android-15.0.0_r0.81
+    [kernel_google-modules_amplifiers]=android-15.0.0_r0.81
+    [kernel_google-modules_bms]=android-15.0.0_r0.81
+    [kernel_google-modules_edgetpu_rio]=android-15.0.0_r0.81
+    [kernel_google-modules_gxp_zuma]=android-15.0.0_r0.81
+    [kernel_google-modules_power_reset]=android-15.0.0_r0.81
+    [kernel_google-modules_soc_gs]=android-15.0.0_r0.81
+    [kernel_google-modules_wlan_bcmdhd_bcm4383]=android-15.0.0_r0.81
 )
 
 declare -Ar kernel_tags=(
     # April 2025
-    [kernel_build]=android-15.0.0_r0.79
-    [kernel_devices_google_akita]=android-15.0.0_r0.79
-    [kernel_devices_google_bluejay]=android-15.0.0_r0.79
-    [kernel_devices_google_caimito]=android-15.0.0_r0.79
-    [kernel_devices_google_comet]=android-15.0.0_r0.79
-    [kernel_devices_google_felix]=android-15.0.0_r0.79
-    [kernel_devices_google_lynx]=android-15.0.0_r0.79
-    [kernel_devices_google_pantah]=android-15.0.0_r0.79
-    [kernel_devices_google_raviole]=android-15.0.0_r0.79
-    [kernel_devices_google_shusky]=android-15.0.0_r0.79
-    [kernel_devices_google_tangorpro]=android-15.0.0_r0.79
-    [kernel_google-modules_amplifiers]=android-15.0.0_r0.79
-    [kernel_google-modules_bms]=android-15.0.0_r0.79
-    [kernel_google-modules_edgetpu_rio]=android-15.0.0_r0.79
-    [kernel_google-modules_gxp_gs201]=android-15.0.0_r0.79
-    [kernel_google-modules_gxp_zuma]=android-15.0.0_r0.79
-    [kernel_google-modules_power_reset]=android-15.0.0_r0.79
-    [kernel_google-modules_soc_gs]=android-15.0.0_r0.79
-    [kernel_google-modules_wlan_bcmdhd_bcm4383]=android-15.0.0_r0.79
-    [kernel_google-modules_wlan_bcmdhd_bcm4389]=android-15.0.0_r0.79
-    [kernel_google-modules_wlan_bcmdhd_bcm4390]=android-15.0.0_r0.79
-    [kernel_google-modules_wlan_bcmdhd_bcm4398]=android-15.0.0_r0.79
-    [kernel_google-modules_wlan_syna_dhd43752p]=android-15.0.0_r0.79
+    [kernel_build]=android-15.0.0_r0.81
+    [kernel_devices_google_tegu]=android-15.0.0_r0.81
+    [kernel_google-modules_amplifiers]=android-15.0.0_r0.81
+    [kernel_google-modules_bms]=android-15.0.0_r0.81
+    [kernel_google-modules_edgetpu_rio]=android-15.0.0_r0.81
+    [kernel_google-modules_gxp_zuma]=android-15.0.0_r0.81
+    [kernel_google-modules_power_reset]=android-15.0.0_r0.81
+    [kernel_google-modules_soc_gs]=android-15.0.0_r0.81
+    [kernel_google-modules_wlan_bcmdhd_bcm4383]=android-15.0.0_r0.81
 )
 
 readonly independent=(
     adevtool
     branding
-    device_google_akita-kernels_6.1
-    device_google_bluejay-kernels_6.1
-    device_google_caimito-kernels_6.1
-    device_google_comet-kernels_6.1
-    device_google_felix-kernels_6.1
-    device_google_lynx-kernels_6.1
-    device_google_pantah-kernels_6.1
-    device_google_raviole-kernels_6.1
-    device_google_shusky-kernels_6.1
-    device_google_tangorpro-kernels_6.1
+    #device_google_akita-kernels_6.1
+    #device_google_bluejay-kernels_6.1
+    #device_google_caimito-kernels_6.1
+    #device_google_comet-kernels_6.1
+    #device_google_felix-kernels_6.1
+    #device_google_lynx-kernels_6.1
+    #device_google_pantah-kernels_6.1
+    #device_google_raviole-kernels_6.1
+    #device_google_shusky-kernels_6.1
+    #device_google_tangorpro-kernels_6.1
     hardened_malloc
     kernel_common-6.1
     kernel_common-6.6
