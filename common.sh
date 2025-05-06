9b3bc98e692a83e325affeefd5079423182bf3a4
Daniel Micay <daniel.micay@grapheneos.org>
2025-05-06 17:38:16 -0400

    update to BD4A.250505.003

--- common.sh
+++ common.sh
@@ -1,6 +1,6 @@
 readonly branch=15-qpr1-tegu
-readonly aosp_tag_old=android-15.0.0_r25
-readonly aosp_tag=android-15.0.0_r25
+readonly aosp_tag_old=android-15.0.0_r31
+readonly aosp_tag=android-15.0.0_r31
 
 user_error() {
     echo $1 >&2
