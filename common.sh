dc31206a45a4140e985695f1b552fecec98673af
Daniel Micay <daniel.micay@grapheneos.org>
2025-06-02 15:03:35 -0400

    add back platform_frameworks_av

--- common.sh
+++ common.sh
@@ -40,6 +40,7 @@ readonly aosp_forks=(
     platform_external_conscrypt
     platform_external_robolectric
     platform_external_selinux
+    platform_frameworks_av
     platform_frameworks_base
     platform_frameworks_libs_systemui
     platform_frameworks_native
