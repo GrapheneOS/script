5de0aeda5ac6cfd235b95edfdbdbfd6c1439f1cb
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-08-17 23:34:33 +0300

    add platform_build_blueprint

--- common.sh
+++ common.sh
@@ -14,6 +14,7 @@ readonly aosp_forks=(
     platform_bionic
     platform_bootable_recovery
     platform_build
+    platform_build_blueprint
     platform_build_release
     platform_build_soong
     platform_development
