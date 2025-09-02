52e3539b926633da4d72427222c57b44a8cc04dd
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-09-02 20:36:11 +0300

    add platform_system_apex

--- common.sh
+++ common.sh
@@ -67,6 +67,7 @@ readonly aosp_forks=(
     platform_packages_services_Mms
     platform_packages_services_Telecomm
     platform_packages_services_Telephony
+    platform_system_apex
     platform_system_ca-certificates
     platform_system_core
     platform_system_extras
