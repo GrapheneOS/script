c780c1dc50d3dcb5f1de00d60c6f5a324b694f27
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-02-12 15:38:48 +0000

    finalize: use out-dir-aware otatools source location

--- finalize.sh
+++ finalize.sh
@@ -11,6 +11,6 @@ source "$(dirname ${BASH_SOURCE[0]})/common.sh"
 
 readonly releases=releases/$BUILD_NUMBER
 mkdir -p $releases
-otatools="${ANDROID_BUILD_TOP}/out/soong/.intermediates/build/make/tools/otatools_package/otatools-package/linux_glibc_x86_64/gen/otatools.zip"
+otatools="${ANDROID_HOST_OUT}/obj/ETC/otatools-packagelinux_glibc_x86_64_intermediates/otatools-packagelinux_glibc_x86_64"
 cp "${otatools}" "$releases/$TARGET_PRODUCT-otatools.zip"
 cp "$OUT/obj/PACKAGING/target_files_intermediates/$TARGET_PRODUCT-target_files.zip" "$releases/"
