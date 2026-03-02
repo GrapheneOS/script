a1edd8fe4f6c3aa8d62495a33cb9c54671117f43
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-03-02 19:06:35 +0000

    add platform_external_dng_sdk

--- common.sh
+++ common.sh
@@ -18,6 +18,7 @@ readonly aosp_forks=(
     platform_build_soong
     platform_development
     platform_external_conscrypt
+    platform_external_dng_sdk
     platform_external_libpng
     platform_external_selinux
     platform_external_sqlite
