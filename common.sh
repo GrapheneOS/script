33bcf44fdb48552906f412740ea1231fae794c7f
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-03-02 19:06:42 +0000

    add platform_hardware_st_nfc

--- common.sh
+++ common.sh
@@ -27,6 +27,7 @@ readonly aosp_forks=(
     platform_frameworks_native
     platform_frameworks_opt_net_wifi
     platform_frameworks_opt_telephony
+    platform_hardware_st_nfc
     platform_libcore
     platform_packages_apps_Calendar
     platform_packages_apps_CellBroadcastReceiver
