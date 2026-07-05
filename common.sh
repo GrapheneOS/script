2695890ba592bd436158066efd99c5a7a91a957f
Daniel Micay <daniel.micay@grapheneos.org>
2026-07-04 21:35:51 -0400

    Revert "add platform_system_fs_fs_mgr"
    This reverts commit 5c114eb978d8f61869945e79c586ccb4025d7e13.

--- common.sh
+++ common.sh
@@ -69,7 +69,6 @@ readonly aosp_forks=(
     platform_system_ca-certificates
     platform_system_core
     platform_system_extras
-    platform_system_fs_fs_mgr
     platform_system_logging
     platform_system_netd
     platform_system_sepolicy
