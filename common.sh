639cdf6558e7401f8bab1cb8f53546ff4a0c8fef
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-09-09 15:43:27 +0000

    add platform_system_libufdt

--- common.sh
+++ common.sh
@@ -82,6 +82,7 @@ readonly aosp_forks=(
     platform_system_extras
     platform_system_fs_fs_mgr
     platform_system_libfmq
+    platform_system_libufdt
     platform_system_logging
     platform_system_netd
     platform_system_sepolicy
