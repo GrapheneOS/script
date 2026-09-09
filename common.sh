cff1784f9798c36fe380660bf60aee72e645a130
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-09-09 15:43:27 +0000

    add platform_hardware_interfaces

--- common.sh
+++ common.sh
@@ -34,6 +34,7 @@ readonly aosp_forks=(
     platform_frameworks_native
     platform_frameworks_opt_net_wifi
     platform_frameworks_opt_telephony
+    platform_hardware_interfaces
     platform_libcore
     platform_packages_apps_Calendar
     platform_packages_apps_CellBroadcastReceiver
